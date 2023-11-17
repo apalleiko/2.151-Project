a9052c85660bf4e151d243b3dac45d9542573ed7
apalleiko <67287383+apalleiko@users.noreply.github.com>
2023-11-17 13:46:21 -0500

    Updated linearization setup

--- linearize_around_state.m
+++ linearize_around_state.m
@@ -1,10 +1,15 @@
-function [A,B] = linearize_around_state(M,C,B)
-    % Linearize manipulator equations around nominal state q:
-    % M(q)*q_ddot+C(q)*q_dot = B*torques + g(q)
+function [A,B] = linearize_around_state(M,C,B,g,q)
+    % Linearize manipulator equations around nominal joint positions q(t).
+    % q(t) should be a vector of joint states as a function of time, eg.
+    % [theta_1(t); theta_2(t),...]
+    % See test_functions.mlx for an example
+
+    % This function assumes that M, C, B, and g are symbolic functions of
+    % q(t):  M(q)*q_ddot+C(q,q_dot)*q_dot = B*torques + g(q)
     
-    % q is vector of joint positions
     % returns A,B matrices for statespace equations of x = [q, q_dot] and
     % u = [torques]
+    out = 
 
     n = size(M,1); % n joint states
     assert(n == size(B,2)); % should be n control torques
