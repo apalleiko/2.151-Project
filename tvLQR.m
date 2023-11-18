5876d7c45744de5794a2b2a0c84286fb046f22ba
Mohamad Alrished <alrished@31-35-193.wireless.csail.mit.edu>
2023-11-17 19:55:54 -0500

    minor update

--- tvLQR.m
+++ tvLQR.m
@@ -5,6 +5,7 @@ function K_t = tvLQR(A,B,Q,R)
 forwardKinematics = @(x) [cos(x), -sin(x); sin(x), cos(x)];  
 
 tspan = linspace(0, 1, 100);  
+K_t = zeros(size(tspan,2),1);
 
 for i = 1:length(tspan)
     t = tspan(i);
@@ -21,7 +22,7 @@ for i = 1:length(tspan)
     % i should use icare will update 
     P_t = care(A_augmented, B_augmented, Q, R);
     
-    K_t = -B_augmented' * P_t;
+    K_t[i] = -B_augmented' * P_t;
     
 end
 end
