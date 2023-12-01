3ef9779637b136847f237da01589eeda1f1025d1
apalleiko <67287383+apalleiko@users.noreply.github.com>
2023-12-01 16:58:09 -0500

    Got a basic trajectory working

--- create_robot_tree.m
+++ create_robot_tree.m
@@ -1,28 +1,42 @@
-function [robot] = create_robot_tree()
-% Create associated robot tree to visualize things
+function [robot] = create_robot_tree(L)
+% Create associated robot tree as robot_arm_dynamics.m to visualize things 
+% L is the length of each arm
 robot = rigidBodyTree("DataFormat","column");
 base = robot.Base;
 arm1 = rigidBody("arm1");
 arm2 = rigidBody("arm2");
+arm3 = rigidBody("arm3");
 
 jnt1 = rigidBodyJoint("jnt1","revolute");
 jnt2 = rigidBodyJoint("jnt2","revolute");
+jnt3 = rigidBodyJoint("jnt3","fixed");
 
 jnt1.JointAxis = [1 0 0]; % y-axis
-jnt1.HomePosition = pi/4;
+% jnt1.HomePosition = pi/4;
+jnt1.HomePosition = 0;
 jnt2.JointAxis = [1 0 0];
-jnt2.HomePosition = pi/4;
+% jnt2.HomePosition = pi/4;
+jnt2.HomePosition = 0;
 
-setFixedTransform(jnt1,trvec2tform([0 1 0]))
-setFixedTransform(jnt2,trvec2tform([0 1 0]))
+j_vec = [0 L 0];
+
+setFixedTransform(jnt1,trvec2tform([0 0 0]))
+setFixedTransform(jnt2,trvec2tform(j_vec))
+setFixedTransform(jnt3,trvec2tform(j_vec))
 
 arm1.Joint = jnt1;
+arm1.CenterOfMass = j_vec/2;
 arm2.Joint = jnt2;
+arm2.CenterOfMass = j_vec/2;
+arm3.Joint = jnt3;
 
 addBody(robot,arm1,"base");
 addBody(robot,arm2,"arm1");
+addBody(robot,arm3,"arm2");
 
 showdetails(robot)
 show(robot)
+xlim([-.01,.01])
+view(90,0)
 end
 
--- main.m
+++
@@ -1,5 +0,0 @@
-clear, clc
-[q_ddot, q, u, vars] = robot_arm_dynamics();
-[A,B,dq,du] = linearize_around_state(q_ddot,q,u);
-%%
-robot = create_robot_tree();
