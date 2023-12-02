a7491cd59cd24e5131f0f0ab2ec4c09ebe8aa3e9
apalleiko <67287383+apalleiko@users.noreply.github.com>
2023-12-02 15:18:31 -0500

    created numerical trajectory methods for the robot

---
+++ animate_traj.m
@@ -0,0 +1,16 @@
+function [] = animate_traj(robot,q,rate)
+% Animate trajectory
+count = size(q,2);
+show(robot,q(:,1));
+xlim([-.01,.01])
+view(90,0)
+while true
+    for i = 1:count
+        show(robot,q(:,i),'PreservePlot',false);
+        drawnow
+        pause(rate);
+    end
+    pause(0.5);
+end
+end
+
