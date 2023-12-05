98c235921159367e7d3eb4206d4199e351d75c84
Deanoshuang <102989996+Deanoshuang@users.noreply.github.com>
2023-12-04 19:31:05 -0500

    Robot dynamics worked
    Robot dynamics worked

--- robot_arm_dynamics.m
+++ robot_arm_dynamics.m
@@ -1,35 +1,37 @@
 function [q_ddot, dq, q, u, vars] = robot_arm_dynamics()
-    
     % Define joint states and inputs
-    syms m L t g theta1(t) theta2(t) tau1(t) tau2(t)
-    
+    syms m1 m2 L1 L2 l1 l2 t g theta1(t) theta2(t) tau1(t) tau2(t) 
+    %m1:mass of link1
+    %m2:mass of link2
+    %L1:length of link1
+    %L2:length of link2
+    %l1:center of mass of link1
+    %l2:center of mass of link2
     % Define symbolic variables with it
-    vars = [m L g];
+    vars = [m1 m2 L1 L2 l1 l2 g ];
     
     % Create forward dynamics
-    x1 = L*cos(theta1);
-    y1 = L*sin(theta1);
-    x2 = x1+L*cos(theta1+theta2);
-    y2 = y1+L*sin(theta1+theta2);
-    dx1 = diff(x1,t);
-    dx2 = diff(x2,t);
-    dy1 = diff(y1,t);
-    dy2 = diff(y2,t);
     dtheta1 = diff(theta1,t);
     dtheta2 = diff(theta2,t);
-    I_1 = (1/3)*m*L^2;
-    I_2 = (1/3)*m*L^2;
+    I1 = (1/12)*m1*L1^2;
+    I2 = (1/12)*m2*L2^2;
 
     % Define energies
-    KE=(1/2)*m*(L^2)*((((5/3)+cos(theta2))*(dtheta1^2))+(((2/3)+cos(theta2))*dtheta1*dtheta2+(1/3)*dtheta2^2));
-    P=(0.5*m*g*L*sin(theta1))+(m*g*(L*sin(theta1)+0.5*L*sin(theta1+theta2)));
-    Le = KE-P;
-    
+    %KE of link1
+    KE1=(1/2)*m1*(l1^2)*(dtheta1^2)+(1/2)*I1*dtheta1^2;
+    %PE of link1
+    P1=m1*g*l1*sin(theta1);
+    %KE of link2
+    KE2=(1/2)*m2*((L1^2)*(dtheta1^2)+(l2^2)*((dtheta1+dtheta2)^2)+2*L1*l2*cos(theta2)*(dtheta1*dtheta1+dtheta1*dtheta2))+(1/2)*I2*(dtheta1+dtheta2)^2;
+    %PE of link2 
+    P2=m2*g*(L1*sin(theta1)+l2*sin(theta1+theta2));
+    Le = KE1+KE2-P1-P2;
+   
     q = [theta1; theta2];
     u = [tau1; tau2];
     dq = diff(q,t);
     
-    L_mat = [Le];
+    L_mat = Le;
     dLdq = jacobian(L_mat,q);
     dLdqd = jacobian(L_mat,dq);
     EL = dLdq-diff(dLdqd,t);
